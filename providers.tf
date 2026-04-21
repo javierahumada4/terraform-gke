bfd342156d5af636f141eb4151054265a16f929f
Marcos <202204116@alu.comillas.edu>
2026-04-21 08:41:03 +0200

    Update: providers and variables 🤡

--- providers.tf
+++ providers.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("../icai2025-tofu-397e1ecdd09e.json")
+  credentials = file("../proyecto-tecnologia-2-ac7c23489219.json")
 
   project = var.gcp-project
   region  = var.gcp-region
